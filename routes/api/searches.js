7f733e6aa1d5d47bcb21ed7e30046503978f30d1
Mizanali Panjwani <mizanalip786@gmail.com>
2021-08-17 03:52:32 +0530

    Change search request from GET to POST

--- routes/api/searches.js
+++ routes/api/searches.js
@@ -11,7 +11,7 @@ const auth = require('../../middleware/auth');
 // @route   GET api/searches
 // @desc    Get songs search results
 // @access  Private
-router.get('/', auth, async (req, res) => {
+router.post('/', auth, async (req, res) => {
     try {
         let searchResults = [];
 
